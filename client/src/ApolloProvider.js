ab1ec63bf3e0c64221bce298da894d141e09ecbd
agrsrashti <agrawalsrashti1401@gmail.com>
2022-05-26 21:22:33 +0530

    added deployed link

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -10,7 +10,7 @@ const httpLink = createHttpLink({
   uri:
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
       ? "http://localhost:4000/"
-      : "",
+      : "https://smart-attendance-backend.herokuapp.com",
 });
 
 const authLink = setContext(() => {
